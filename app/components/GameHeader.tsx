723c996234b19e5bb1e80c98c5b0c7e9accc142d
Sadiq Al-Sandouk <sadiqalsandouk@hotmail.com>
2024-12-18 01:51:35 +0000

    shredle logo goes to / page

--- app/components/GameHeader.tsx
+++ app/components/GameHeader.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link"
 import HowToPlay from "./HowToPlay"
 
 export default function GameHeader() {
   return (
     <header className="text-center py-6 mt-5">
       <h1 className="text-3xl sm:text-4xl font-semibold text-green-500 mb-2">
-        🍔 Shredle.com
+        <Link href="/"> 🍔 Shredle.com</Link>
       </h1>
       <p className="text-green-500 sm:text-4xl font-semibold mt-3 text-3xl">
         Which has more calories?
