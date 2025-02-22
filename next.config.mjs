dd38e64c0f0ad7b7056fab72a202b3ff00283a8f
sadiqalsandouk <sadiqalsandouk@hotmail.com>
2025-02-22 01:37:56 +0000

    Revert "connects to mongoDB"
    This reverts commit a846180497c2ef74f518061c3c4c49e2b70dc9e6.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { instrumentationHook: true },
-}
+const nextConfig = {};
 
-export default nextConfig
+export default nextConfig;
