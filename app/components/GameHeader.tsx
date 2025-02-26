ed2319e0791811876bb76466de734c7ece9f5b57
sadiqalsandouk <sadiqalsandouk@hotmail.com>
2025-02-26 03:25:06 +0000

    domain live and changed header

--- app/components/GameHeader.tsx
+++ app/components/GameHeader.tsx
@@ -5,7 +5,7 @@ export default function GameHeader() {
   return (
     <header className="text-center py-6 mt-5">
       <h1 className="text-3xl sm:text-4xl font-semibold text-green-500 mb-2">
-        <Link href="/"> 🍔 Shredle.com</Link>
+        <Link href="/"> 🍔 Shredle.co.uk</Link>
       </h1>
       <p className="text-green-500 sm:text-4xl font-semibold mt-3 text-3xl">
         Which has more calories?
