52bb5e197c92ffe35ba2a724e79b8dbd3296b502
sadiqalsandouk <sadiqalsandouk@hotmail.com>
2025-02-28 00:20:01 +0000

    fixed fisher-yates algo

--- app/utils/fisher-yates.ts
+++ app/utils/fisher-yates.ts
@@ -2,18 +2,20 @@ import { FoodItem } from "../types/types"
 
 export const getDateSeed = () => {
   const today = new Date()
-  return `${today.getFullYear()}-0${today.getMonth() + 1}-${today.getDate()}`
+  const month = (today.getMonth() + 1).toString().padStart(2, "0")
+  const day = today.getDate().toString().padStart(2, "0")
+  return `${today.getFullYear()}-${month}-${day}`
 }
-
 export const seededShuffle = (array: FoodItem[], seed: string) => {
   const shuffled = [...array]
+  let currentHash = 0
   const seededRandom = () => {
-    let hash = 0
-    for (let i = 0; i < seed.length; i++) {
-      hash = (hash << 5) - hash + seed.charCodeAt(i)
-      hash = hash & hash
-    }
-    return Math.abs(hash) / Math.pow(2, 32)
+    currentHash =
+      (currentHash << 5) -
+      currentHash +
+      seed.charCodeAt(currentHash % seed.length)
+    currentHash = currentHash & currentHash
+    return Math.abs(currentHash) / Math.pow(2, 32)
   }
 
   for (let i = shuffled.length - 1; i > 0; i--) {
