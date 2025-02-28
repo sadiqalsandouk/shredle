1bab12a585c86b4fa52b77b0efd46ee02a330abe
sadiqalsandouk <sadiqalsandouk@hotmail.com>
2025-02-28 00:26:13 +0000

    minor style changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,12 +3,20 @@ import React from "react"
 
 export default function Footer() {
   return (
-    <footer className="text-black py-4">
-      <div className="container mx-auto flex justify-center space-x-4">
-        <Link href="/">Play</Link>
-        <Link href="/privacy">Privacy Policy</Link>
-        <Link href="/terms">Terms & Conditions</Link>
-        <Link href="/contact">Contact Us</Link>
+    <footer className="text-black py-4 px-4">
+      <div className="container mx-auto flex flex-row flex-wrap justify-center gap-4">
+        <Link className="hover:text-gray-600" href="/">
+          Play
+        </Link>
+        <Link className="hover:text-gray-600" href="/privacy">
+          Privacy Policy
+        </Link>
+        <Link className="hover:text-gray-600" href="/terms">
+          Terms & Conditions
+        </Link>
+        <Link className="hover:text-gray-600" href="/contact">
+          Contact Us
+        </Link>
       </div>
     </footer>
   )
--- app/components/GameHeader.tsx
+++ app/components/GameHeader.tsx
@@ -4,7 +4,7 @@ import HowToPlay from "./HowToPlay"
 export default function GameHeader() {
   return (
     <header className="text-center py-6 mt-5">
-      <h1 className="text-3xl sm:text-4xl font-semibold text-green-500 mb-2">
+      <h1 className="hover:text-green-600 text-3xl sm:text-4xl font-semibold text-green-500 mb-2">
         <Link href="/"> 🍔 Shredle.co.uk</Link>
       </h1>
       <p className="text-green-500 sm:text-4xl font-semibold mt-3 text-3xl">
